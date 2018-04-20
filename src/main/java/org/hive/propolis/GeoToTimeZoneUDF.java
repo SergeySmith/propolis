1fc14529c99d0b0000ff609a9c256864384ac37d
Sergey Kuznetsov <s.kuznetsov@corp.mail.ru>
2018-04-20 10:58:35 +0300

    UDF to get timezone from given geo coordinates

---
+++ src/main/java/org/hive/propolis/GeoToTimeZoneUDF.java
@@ -0,0 +1,84 @@
+package org.hive.propolis;
+
+import net.iakovlev.timeshape.TimeZoneEngine;
+import org.apache.hadoop.hive.ql.exec.*;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.DoubleObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.slf4j.LoggerFactory;
+
+import java.time.ZoneId;
+
+@Description(
+        name = "Get timezone from geo coordinates (lat, lon)",
+        value = "_FUNC_(lat Double, lon Double): String\n"
+                + "-- finds timezone from a given (latitude, longitude) pair"
+)
+
+public class GeoToTimeZoneUDF extends GenericUDF {
+
+    private static final int NUM_ARGS = 2;
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(GeoToTimeZoneUDF.class.getName());
+    private TimeZoneEngine engine;
+
+    private transient DoubleObjectInspector latOI;
+    private transient DoubleObjectInspector lonOI;
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
+        if (arguments.length != NUM_ARGS) {
+            throw new UDFArgumentLengthException(
+                    "GeoToTimeZoneUDF accepts exactly " + NUM_ARGS + " arguments.");
+        }
+
+        for (int i = 0; i < NUM_ARGS; ++i) {
+            if (arguments[i].getCategory() != ObjectInspector.Category.PRIMITIVE) {
+                throw new UDFArgumentTypeException(i,
+                        "The " + (i+1) + "th argument must be Primitive, but "
+                                + arguments[i].getCategory().name()
+                                + " was passed.");
+            }
+        }
+
+        latOI = (DoubleObjectInspector) arguments[0];
+        lonOI = (DoubleObjectInspector) arguments[1];
+
+        return PrimitiveObjectInspectorFactory.javaStringObjectInspector;
+    }
+
+    @Override
+    public void configure(MapredContext context) {
+        super.configure(context);
+        LOG.warn("[configure] Initializing TimeZoneEngine");
+        engine = TimeZoneEngine.initialize();
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[] arguments) throws HiveException {
+        Object latObject = arguments[0].get();
+        Object lonObject = arguments[1].get();
+
+        if (latObject == null || lonObject == null) {
+            return null;
+        }
+
+        Double lat = (Double) latOI.getPrimitiveJavaObject(latObject);
+        Double lon = (Double) lonOI.getPrimitiveJavaObject(lonObject);
+
+        if (engine == null) {
+            LOG.warn("[eval] Initializing TimeZoneEngine");
+            engine = TimeZoneEngine.initialize();
+        }
+
+        ZoneId zoneId = engine.query(lat, lon).get();
+        return zoneId.getId();
+    }
+
+    @Override
+    public String getDisplayString(String[] children) {
+        assert (children.length == NUM_ARGS);
+        return getStandardDisplayString("timezone", children);
+    }
+}
---
+++ src/test/java/org/hive/propolis/GeoToTimeZoneUDFTest.java
@@ -0,0 +1,62 @@
+package org.hive.propolis;
+
+import com.klarna.hiverunner.HiveShell;
+import com.klarna.hiverunner.StandaloneHiveRunner;
+import com.klarna.hiverunner.annotations.HiveSQL;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(StandaloneHiveRunner.class)
+public class GeoToTimeZoneUDFTest {
+
+    @Rule
+    public TestName name = new TestName();
+
+    @HiveSQL(files = {})
+    private HiveShell shell;
+
+    @Test
+    public void test_1() {
+        List<Object[]> expected = shell.executeStatement("select 'Europe/Berlin' as tz "
+                + "union all "
+                + "select null as tz "
+                + "union all "
+                + "select 'Europe/Moscow' as tz "
+        );
+
+        shell.execute( "create temporary function "
+                + "timezone as 'org.hive.propolis.GeoToTimeZoneUDF';"
+        );
+
+        List<Object[]> result = shell.executeStatement("with temp_table as ( "
+                + "select 52.52 as lat, 13.40 as lon "
+                + "union all "
+                + "select null as lat, 1.01 as lon "
+                + "union all "
+                + "select 56.52 as lat, 50.40 as lon ) "
+                + "select timezone(lat, lon) as tz from temp_table");
+        printResult(result);
+
+        assert(expected.size() == 3);
+        assertEquals(expected.size(), result.size());
+        for (int i = 0; i < result.size(); ++i) {
+            assertArrayEquals(expected.get(i), result.get(i));
+        }
+    }
+
+    public void printResult(List<Object[]> result) {
+        System.out.println(String.format("Result from %s:",name.getMethodName()));
+        for (Object[] row : result) {
+            System.out.println(Arrays.asList(row));
+        }
+    }
+}
